e6b6c83065a5539225035341db87ee3c183ec757
hoya838 <max1004s@naver.com>
2025-09-09 10:19:48 +0900

    수정

--- src/components/AboutInfo.jsx
+++ src/components/AboutInfo.jsx
@@ -24,10 +24,10 @@ function Aboutinfo() {
           </div>
 
           <div className="ab-right">
-            <p className="text-wrapper-body-right vi-p">
+            <p className="text-wrapper-body-right vi-rec">
               {filteredData[0].descriptionKo}
             </p>
-            <p className="text-wrapper-body-right vi-p2">
+            <p className="text-wrapper-body-right vi-rec2">
               {filteredData[0].descriptionEn}
             </p>
           </div>
@@ -40,28 +40,17 @@ function Aboutinfo() {
         <div className="text-wrapper">{filteredData[0].menuKo}</div>
         <div className="text-wrapper-2">{filteredData[0].menuEn}</div>
         <div className="content-row-vi">
-          <img src="/img/about1.png" alt="UNFOLD THE FLOW" className="visualid_img" />
-          <div className="ab-right">
-            <span className="text-wrapper-body-right vi-p" style={{ display: "flex", flexDirection: "column", gap: "24px" }}>
-              {filteredData[0].descriptionKo.map((a, i)=>{
-                return (
-                  <div key={i}>
-                    {a}
-                  </div>
-                )
-              })
-              }
-            </span>
-            <span className="text-wrapper-body-right vi-p2" style={{ display: "flex", flexDirection: "column", gap: "24px" }}>
-              {filteredData[0].descriptionEn.map((a, i)=>{
-                return (
-                  <div key={i}>
-                    {a}
-                  </div>
-                )
-              })
-              }
-            </span>
+          <div className="ab-card-img">
+           <img src="/img/about2.png" alt="UNFOLD THE FLOW" className="visualid_img2" />
+            <img src="/img/about3.png" alt="UNFOLD THE FLOW 2" className="visualid_img2" />
+            </div>
+          <div className="ab-right vi-vi">
+            <p className="text-wrapper-body-right vi-p">
+              {filteredData[0].descriptionKo}
+            </p>
+            <p className="text-wrapper-body-right vi-p2">
+              {filteredData[0].descriptionEn}
+            </p>
           </div>
         </div>
       </>
@@ -72,28 +61,12 @@ function Aboutinfo() {
         <div className="text-wrapper">{filteredData[0].menuKo}</div>
         <div className="text-wrapper-2">{filteredData[0].menuEn}</div>
         <div className="content-row-3">
-          <span className="text-wrapper-body-left" style={{ display: "flex", flexDirection: "column", gap: "24px" }}>
-            {
-              filteredData[0].descriptionKo.map((a, i)=>{
-                return (
-                  <div key={i}>
-                    {a}
-                  </div>
-                )
-              })
-            }
-          </span>
-          <span className="text-wrapper-body-right"style={{ display: "flex", flexDirection: "column", gap: "24px" }}>
-            {
-              filteredData[0].descriptionEn.map((a, i)=>{
-                return (
-                  <div key={i}>
-                    {a}
-                  </div>
-                )
-              })
-            }
-          </span>
+          <p className="text-wrapper-body-left">
+            {filteredData[0].descriptionKo}
+          </p>
+          <p className="text-wrapper-body-right">
+            {filteredData[0].descriptionEn}
+          </p>
         </div>
       </>
     )
